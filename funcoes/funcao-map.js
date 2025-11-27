e849009b906df180bd00a550b9fb640df21ac476
efranco-dev <evanvidel@gmail.com>
2025-11-27 11:05:41 -0300

    revisao de objeto

---
+++ funcoes/funcao-map.js
@@ -0,0 +1,33 @@
+//Dobre os numeros
+
+const numeros = [5, 50, 80, 1, 2, 3, 4, 5, 6, 7, 11, 15, 22, 27];
+
+/* const numerosEmDobro = numeros.map((valor) => valor * 2);
+console.log(numerosEmDobro);
+ */
+const pessoas = [
+  { nome: 'Luiz', idade: 62 },
+  { nome: 'Maria', idade: 23 },
+  { nome: 'Eduardo', idade: 55 },
+  { nome: 'Letícia', idade: 19 },
+  { nome: 'Rosana', idade: 32 },
+  { nome: 'Wallace', idade: 47 },
+];
+
+const nomes = pessoas.map((obj) => obj.nome);
+
+const idades = pessoas.map((obj) => ({ idade: obj.idade }));
+
+const comIds = pessoas.map(function (obj, indice) {
+  const newObj = {...obj}
+  newObj.ide = indice
+  return newObj
+  /* obj.id = (indice + 1) * 1000;
+  return obj; */
+});
+console.log(pessoas);
+console.log(comIds);
+
+/* console.log(idades);
+console.log(comIds);
+ */
